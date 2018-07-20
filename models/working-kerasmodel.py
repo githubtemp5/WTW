8ae402cb3614cb80b3b96080fd12d086651d610d
Muklek <32003500+Muklek@users.noreply.github.com>
2018-07-20 23:07:11 +0100

    sample model using keras

---
+++ models/working-kerasmodel.py
@@ -0,0 +1,92 @@
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+
+# load dataset
+dataframe = pandas.read_csv('C:\\Users\\mukle\\Documents\\python-ml\\dataset\\1.txt',
+                            header=None,
+                            error_bad_lines=False,
+                            sep='\s+')
+
+dataframe.columns = ['A','B']
+X = dataframe['A']
+Y = dataframe['B']
+print(X)
+print("##########")
+print(Y)
+
+def baseline_model():
+	model = Sequential()
+	model.add(Dense(1, input_dim=1, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
+
+print("starting")
+seed = 7
+numpy.random.seed(seed)
+estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=1, verbose=0)
+
+print("passed first round")
+kfold = KFold(n_splits=2, random_state=seed)
+results = cross_val_score(estimator, X, Y, cv=kfold)
+print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+print("Almost there...")
+# evaluate model with standardized dataset
+numpy.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasRegressor(build_fn=baseline_model, epochs=50, batch_size=1, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = KFold(n_splits=2, random_state=seed)
+results = cross_val_score(pipeline, X, Y, cv=kfold)
+print("Standardized: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+"""
+# define the model
+def larger_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(6, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
+
+numpy.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasRegressor(build_fn=larger_model, epochs=50, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = KFold(n_splits=10, random_state=seed)
+results = cross_val_score(pipeline, X, Y, cv=kfold)
+print("Larger: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+def wider_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
+
+
+numpy.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = KFold(n_splits=10, random_state=seed)
+results = cross_val_score(pipeline, X, Y, cv=kfold)
+print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+"""
