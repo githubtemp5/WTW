55b424b5b3dfda91c19a575ec6d431fdadfb8d96
githubtemp5 <30862041+githubtemp5@users.noreply.github.com>
2018-07-24 13:26:16 +0100

    Def Working
    Def Working

--- Alvin Class Files/kerasRegression.py
+++ Alvin Class Files/kerasRegression.py
@@ -2,18 +2,21 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation
 from keras.optimizers import SGD
 
 __main__ = "__main__"
+features = ['Existing book', 'New Business']
+output = 'Sales'
+file_path = "PMI_dataset_large.csv"
 
 #sns.lmplot(data = dataframe, x='x1', y='x2', hue='y', fit_reg=False)
 #plt.show()
 
+
+
 #preprocessing data
-def preprocess(X_train, X_test):
+def preprocess(X_train, X_test, y_train, y_test):
     mean = X_train.mean(axis=0)
     X_train -= mean
     std = X_train.std(axis=0)
@@ -24,41 +27,56 @@ def preprocess(X_train, X_test):
     std = X_test.std(axis=0)
     X_test /= std
     
-    return X_train, X_test
+    mean = y_train.mean(axis=0)
+    y_train -= mean
+    std = y_train.std(axis=0)
+    y_train /= std
+    
+    mean = y_test.mean(axis=0)
+    y_test -= mean
+    std = y_test.std(axis=0)
+    y_test /= std
+    
+    return X_train, X_test, y_train, y_test
     
 def initialiseModel():
     
     #initialising a model
-    model = Sequential('Regression_Model')
-    model.add(layers.Dense(name="Input_Layer", units=3, input_dim=2))
-    model.add(layers.Activation(name='rel', activation='relu'))
+    model = Sequential(name='Regression_Model')
+    
+    #input layer
+    model.add(layers.Dense(name="Layer1", units=3, input_dim=len(features), activation = 'relu'))
     model.summary()
     
-    #stochastic gradient descent, learning rate
+    #output layer
+    model.add(layers.Dense(name='OutputLater', units=1))
+    model.add(layers.Activation(name='sig', activation='sigmoid'))
+    
+    #stochastic gradient descent, learning rate decay
     stogd = SGD(0.01)
     
     #compiling the model, telling keras what to minimise.
     model.compile(loss='mse', optimizer=stogd, metrics=['mse'])
 
-#train the model
-#model.fit(X_train, y_train, epochs=100, batch_size =1)
-
-#prediction = model.predict(dataframe.values)
+    #train the model
+    model.fit(X_train, y_train, batch_size=10, epochs = 100, validation_split = 0.1)
+    
 
 if(__main__ == "__main__"):
 
-    dataframe = pd.read_csv("moons.csv", sep=',')
-
+    dataframe = pd.read_csv(file_path , sep=',')
+    
+    train_index = dataframe.sample(frac=0.6, random_state=21).index
     #assigning training data
-    X_train, y_train = dataframe.iloc[train_index][['x1','x2']],dataframe.iloc[train_index]['y']
+    X_train, y_train = dataframe.iloc[train_index][features],dataframe.iloc[train_index][output]
         
     #assigning testing data
-    X_test, y_test = dataframe.drop(index=train_index)[['x1','x2']],dataframe.drop(index=train_index)['y']
+    X_test, y_test,  = dataframe.drop(index=train_index)[features],dataframe.drop(index=train_index)[output]
     
     #initialising tensorboard
     tensorboard = TensorBoard(log_dir="C:\\Users\\ShresthaAl\\Documents\\results")
     
     print(X_train)
-    X_train, X_test = preprocess(X_train, X_test)
+    X_train, X_test, y_train, y_test = preprocess(X_train, X_test, y_train, y_test)
     print(X_train)
-   #initialiseModel()
+    initialiseModel()
