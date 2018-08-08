8c4f00b7ecdd2787167b63e7d5ae5b2f3ec5141d
githubtemp5 <30862041+githubtemp5@users.noreply.github.com>
2018-08-08 22:26:57 +0100

    Working model, bit fiddly with batch sizes

--- Alvin Class Files/overNight4.py
+++ Alvin Class Files/overNight4.py
@@ -13,34 +13,39 @@ from keras.callbacks import TensorBoard
 from keras.optimizers import RMSprop, Adam
 from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_csv("C:\\Users\\ShresthaAl\\Documents\\Datasets\\PMI_dataset_large_updated_lossBusiness.csv", dtype={'Year':str, 'Month':str})
+df = pd.read_csv("C:\\Users\\ShresthaAl\\Downloads\\Appliances-energy-prediction-data-master\\Appliances-energy-prediction-data-master\\training.csv", dtype={'WeekStatus':str, 'Day_of_week':str})#'Year':str, 'Month':str}
 
-df2 = pd.read_csv("C:\\Users\\ShresthaAl\\Documents\\Datasets\\PMI_dataset_large_updated_lossBusiness3.csv")
+#df2 = pd.read_csv("C:\\Users\\ShresthaAl\\Documents\\Datasets\\PMI_dataset_large_updated_lossBusiness3.csv")
 print(df.describe())
 
+#preprocessing data
+#converting dates to 0 and 1 bag of words technique?
 dataframe = pd.get_dummies(df)
 
+#dataframe to numpy array
 dataframe_np = dataframe.values
 
 d_len = len(dataframe)
-d_test_split = d_len-10
+d_test_split = d_len-14003
 d_columns = dataframe.shape[1]
 
+#scale and assign test and training data for target(y)
 y = dataframe_np[:,0]
 y = y.reshape(d_len,1)
 scaler = MinMaxScaler(feature_range=(-1,1))
 y= scaler.fit_transform(y)
 y= y.reshape(d_len,1)
 y = y.reshape(d_len,1,1)
-y_test = y[d_test_split:]
+y_test = y[d_test_split:810]
 y = y[:d_test_split]
 
 
+#scale and assign test and training data for features(x)
 x = dataframe_np[:,1:]
 sc = MinMaxScaler(feature_range=(-1,1))
 #x = sc.fit_transform(x)
 x = x.reshape(d_len,1,d_columns-1)
-x_test = x[d_test_split:]
+x_test = x[d_test_split:810]
 x = x[:d_test_split]
 
 # mean = y.mean(axis=0)
@@ -51,47 +56,50 @@ x = x[:d_test_split]
 import random
 lis=[]
 
-d = 0.15
-l = 1
-lr = 0.001
-b_size = 3
-e = 300
-lis.append((d,l,lr,b_size,e))
+drop_rate = 0.1
+neuron_units = 256
+l_rate = 0.001
+b_size = 10
+ep = 20
+lis.append((drop_rate,neuron_units,l_rate,b_size,ep))
 print(lis)
 
-for (d,l,lr,b_size,e) in lis:
+for (drop_rate,neuron_units,l_rate,b_size,ep) in lis:
     #initialising a model
-    input_layer = Input(name='input_layer',shape=(None,x.shape[-1]))
+    input_layer = Input(name='input_layer', shape=(None,x.shape[-1]), batch_shape=(b_size,1,x.shape[2]))
     
     #hidden layers
-    lstm_1 = LSTM(l,name='lstm_1',dropout=d,recurrent_dropout=0, return_sequences=True)(input_layer)
-    lstm_2 = LSTM(l,name='lstm_2',dropout=d,recurrent_dropout=0, return_sequences=True)(lstm_1)#, return_sequences=True
+    lstm_1 = LSTM(neuron_units,name='lstm_1',dropout=drop_rate,recurrent_dropout=0, stateful=True, return_sequences=True)(input_layer)
+    lstm_2 = LSTM(neuron_units,name='lstm_2',dropout=drop_rate,recurrent_dropout=0,return_sequences=True, stateful=True)(lstm_1)#
 
     #output layer
     output_layer = Dense(1,name='out_1')(lstm_2)
     model = Model(input_layer, output_layer)
-    model.compile(optimizer = Adam(lr=lr), loss='mse')
+    model.compile(optimizer = Adam(lr=l_rate), loss='mse')
 
-    fit = model.fit(x, y,batch_size=b_size, epochs=e, verbose=1, validation_split=0.1, shuffle=True)
+    #training the model
+    fit = model.fit(x, y, batch_size=b_size, epochs=ep, verbose=1, validation_split=0.1, shuffle=True)
+    
+    #plotting the validation and loss graph
     loss = fit.history['loss']
     val_loss = fit.history['val_loss']
     epochs = range(1, len(loss) + 1)
     plt.figure()
-    
     plt.plot(epochs, loss, 'bo', label='Training loss')
     plt.plot(epochs, val_loss, 'b', label='Validation loss')
     plt.title('Training and Validation loss')
     plt.legend()
     plt.show()
-    oo=str.replace(('picPredictLostB3'+','+str(d)+','+str(l)+','+str(lr)+','+str(b_size)+','+str(e)+'BBBBBAAA'),'.','_')
+    
+    oo=str.replace(('picDateSales5'+';'+str(drop_rate)+';'+str(neuron_units)+';'+str(l_rate)+';'+str(b_size)+';'+str(ep)),'.','_')
     plt.savefig(oo)
     
-    preds = model.predict(x_test, steps=1)
+    preds = model.predict(x_test, batch_size=b_size )#steps=1
     for i in range(len(x_test)):
         print( "X=%s, Predicted=%s" % (scaler.inverse_transform(y_test[i]),scaler.inverse_transform(preds[i])))
        # print(math.sqrt(preds[i]))
     
-    model.save('modelLost3'+','+str(d)+','+str(l)+','+str(lr)+','+str(b_size)+','+str(e)+'BBBBBAAA')
+    model.save('modelTemp6'+';'+str(drop_rate)+';'+str(neuron_units)+';'+str(l_rate)+';'+str(b_size)+';'+str(ep))
     
 def plotInfo():    
     #sns.lmplot(data = dataframe, x='Sales', y='Existing book', hue='Sales', fit_reg=False)
